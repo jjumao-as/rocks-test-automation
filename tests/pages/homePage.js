d9119f96ec70148488587fbbeaccce26b3d71d12
fsace610 <lmacasaet@fullscale.ph>
2024-06-19 11:30:11 +0800

    rocks-3173-06-19-2024: Active client (My Profile, Contacts, Timezone) (#17)
    * Initial scripts for Active client
    
    * added quicktask dailypreview
    
    * Adding test cases for find talent
    
    * Added missing scenarios for My Profile, Contacts and Timezone update.
    
    ---------
    
    Co-authored-by: Luis Francisco Macasaet <lmacasaet@fullscale.io>
    Co-authored-by: Louie Carlo Rodriguez <lrodriguez@fullscale.ph>

--- tests/pages/homePage.js
+++ tests/pages/homePage.js
@@ -1,4 +1,5 @@
 import { expect } from '@playwright/test';
+const { sleep } = require('../../utils/utility')
 exports.HomePage = class HomePage {
   /**
    * @param {import('@playwright/test').Page} page
@@ -12,26 +13,39 @@ exports.HomePage = class HomePage {
     this.DocumentsAgreementsButton = page.locator('span', {hasText: 'Documents & Agreements'})   
     this.ManageTeamButton = page.locator('span', {hasText: 'Manage Team'})    
     this.ReportingButton = page.locator('span', {hasText: 'Reporting'})    
-    this.NeedHelpButton = page.locator('h4', {hasText: 'Need Help'})     
-   //My Profile Elements
-   this.MyProfileTitle = page.locator('span', { hasText: 'Technical Profile' })
-   this.checkboxWebDevelopment = page.getByRole('checkbox', {name: 'Web Development'})
-   this.checkboxQATesting = page.locator('span', {hasText: 'QA Testing'})
-   this.checkboxMobileDevelopment = page.locator('span', {hasText: 'Mobile Development'})
-   this.webDevelopmentCheckbox = page.locator('label[for="__BVID__60"]')
-   //My Contacts Elements
-   this.addContactButton = page.locator('a', {hasText: 'Add Contact'})
-   this.addContactNameField = page.locator('#text-name')
-   this.addContactEmailField = page.getByRole('email')
-   this.addContactCountryCode = page.locator('placeholder="Country code"')
-   this.addContactPhoneNumber = page.locator('placeholder="Phone number"')
-   this.addContactSaveButton = page.locator('a', {name: 'Save'})
-   this.addContactCancelButton = page.locator('a', {name: 'Cancel'})
-   //Find Talent Elements
-   this.searchField = page.getByPlaceholder('Enter a skill, language, or framework')
-   this.talentDrawerButton = page.getByTitle('Click to open')
-   this.searchButton = page.getByTitle('Search')
-   this.addToTeamButton = page.getByTitle('Add to Team')
+    this.NeedHelpButton = page.locator('h4', {hasText: 'Need Help'})
+    this.homePageButton = page.getByTitle('Go to homepage') 
+    this.dropdownMenu = page.locator('.nav-item.dropdown.ks-user')    
+    this.changeTimeZoneButton = page.locator('span', {hasText: 'Change Timezone'})   
+    //My Profile Elements
+    this.MyProfileTitle = page.locator('span', { hasText: 'Technical Profile' })
+    //----What do you need help checkboxes
+    this.checkboxWebDevelopment = page.locator('#__BVID__60')
+    this.checkboxQATesting = page.locator('#__BVID__61')
+    this.checkboxMobileDevelopment = page.locator('#__BVID__62')
+    //----Which of the following are currently on your team checkboxes
+    this.checkboxProductManager = page.locator('#__BVID__63')
+    this.checkboxProductOwner = page.locator('#__BVID__64')
+    this.checkboxLeadDeveloper = page.locator('#__BVID__65')
+    this.checkboxNone = page.locator('#__BVID__66')
+    //----Other form elements
+    this.otherTechUseField = page.locator('#text-5')
+    this.techUseField = page.locator('.multiselect__select')
+    this.profileSaveButton = page.getByRole('button', {name: 'SAVE'})
+    this.profileCancelButton = page.getByRole('button', {name: 'CANCEL'})
+    //My Contacts Elements
+    this.addContactButton = page.locator('a', {hasText: 'Add Contact'})
+    this.addContactNameField = page.locator('#text-name')
+    this.addContactEmailField = page.locator('input[type="email"]')
+    this.addContactCountryCode = page.locator('placeholder="Country code"')
+    this.addContactPhoneNumber = page.locator('input[type="tel"]')
+    this.addContactSaveButton = page.locator('button', { hastText: 'SAVE'})
+    this.addContactCancelButton = page.locator('button').filter({ hasText: 'CANCEL' })
+    //Find Talent Elements
+    this.searchField = page.getByPlaceholder('Enter a skill, language, or framework')
+    this.talentDrawerButton = page.getByTitle('Click to open')
+    this.searchButton = page.getByTitle('Search')
+    this.addToTeamButton = page.getByTitle('Add to Team')
   }
   // Dynamic elements
   selectTalent (talentName) {
@@ -48,6 +62,24 @@ exports.HomePage = class HomePage {
   async navigateFindTalent () {
     return await this.FindTalentButton.click()
   }
+  async navigateHOme () {
+    await this.homePageButton.click()
+  }
+  async openChangeTimeZone () {
+    await this.dropdownMenu.click();
+    await this.changeTimeZoneButton.click();
+  }
+  async updateTimeZone (testData) {
+    await this.page.locator('#modalDescription span.selection').click()
+    await this.page.keyboard.type(testData);
+    await this.page.keyboard.press('Enter');
+  }
+  async setTimeZone () {
+    await this.page.getByRole('button', { name: 'Set' }).click()
+  }
+  async closeTimeZoneModal () {
+    await this.page.getByTitle('Press Esc to close').click()
+  }
   // CLIENT PORTAL END OF FUNCTIONS ==================================================================================
   //==================================================================================================================
 
@@ -55,12 +87,43 @@ exports.HomePage = class HomePage {
   //==================================================================================================================
   async udpateProfile (testData) {
     //What do you need help with? section
-    await this.webDevelopmentCheckbox.click()
-    //await this.checkboxWebDevelopment.click()
-    //await this.checkboxQATesting.click()
-    //await this.checkboxMobileDevelopment.click()
-    //await this.techNeededDropdown.locator('visible=true').click()
-    await this.techNeededDropdown.fill('')
+    await this.checkboxWebDevelopment.uncheck( { force: true } )
+    await this.checkboxQATesting.uncheck( { force: true } )
+    await this.checkboxMobileDevelopment.uncheck( { force: true } )
+    await testData.needHelp.forEach(element => {
+      this.page.getByLabel(element).check( { force: true } )
+    });
+    //What tech does or will your project use? section
+    await this.techUseField.click()
+    await this.page.keyboard.press('Backspace');
+    await this.page.keyboard.press('Backspace');
+    await this.page.keyboard.type(testData.techUsed);
+    await sleep(2000)
+    await this.page.locator('li').filter({ hasText: testData.techUsed }).click();
+    await this.techUseField.click()
+    //Other techonologies you use section
+    await this.otherTechUseField.fill(testData.otherTech)
+    //Which of the following are currently on your team? section
+    await sleep(4000)
+    await this.checkboxNone.check({force: true})
+    this.checkboxProductManager.uncheck( { force: true } )
+    this.checkboxProductOwner.uncheck( { force: true } )
+    this.checkboxLeadDeveloper.uncheck( { force: true } )
+    this.checkboxNone.uncheck( { force: true } )
+    await testData.currentlyOnTeam.forEach(element => {
+      if(element === 'None') { this.checkboxNone.check( { force: true } ) }
+      if(element === 'Product Manager') { this.checkboxProductManager.check( { force: true } ) }
+      if(element === 'Lead Developer') { this.checkboxLeadDeveloper.check( { force: true } ) }
+      if(element === 'Product Owner') { this.checkboxProductOwner.check( { force: true } ) }
+    });
+  }
+  async saveUpdates () {
+    await this.profileSaveButton.click()
+    await sleep(4000)
+    
+  }
+  async cancelUpdates () {
+    await this.profileCancelButton.click()
   }
   // MY PROFILE END OF FUNCTIONS =====================================================================================
   //==================================================================================================================
@@ -69,12 +132,12 @@ exports.HomePage = class HomePage {
   //==================================================================================================================
   async openAddContact () {
     await this.addContactButton.click()
-    await expect(page.getByText('Add Contact')).toBeVisible();
+    await expect(this.page.getByRole('heading', { name: 'Add Contact' })).toBeVisible();
   }
   async fillUpContactForm (testData) {
     await this.addContactNameField.fill(testData.name)
     await this.addContactEmailField.fill(testData.email)
-    await this.addContactCountryCode.selectOption(testData.countryCode)
+    //await this.addContactCountryCode.selectOption(testData.countryCode)
     await this.addContactPhoneNumber.fill(testData.phoneNumber)
   }
   async saveContact () {
@@ -83,6 +146,11 @@ exports.HomePage = class HomePage {
   async cancelContact () {
     await this.addContactCancelButton.click()
   }
+  /* TO DO EDIT HAVING TROUBLE CLICKING SPECIFIC EDIT BUTTON ON THE SELECTED CONTACT
+  *async openEditContact() {
+  *  await this.page.locator('//button[contains(@id,"dropdownMenuButton-contact")]').filter(this.page.locator('td', {hasText: 'Sophia Greenholt'})).click()
+  *}
+  */
   // MY CONTACTS END OF FUNCTIONS ====================================================================================
   //==================================================================================================================
 
