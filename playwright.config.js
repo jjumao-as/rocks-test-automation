d1f9299f4f56140589b6493252e181b1491231e5
wdilidili01 <wdilidili@fullscale.ph>
2024-08-21 11:16:10 +0800

    added book a call

--- playwright.config.js
+++ playwright.config.js
@@ -56,7 +56,7 @@ module.exports = defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'], headless: false},
     },
 
     {
