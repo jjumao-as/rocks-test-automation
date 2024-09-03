198fabea985e4d6ab03ecc2b73d0c15686571ccd
unknown <ndulin@fullscale.ph>
2024-09-03 10:28:05 +0800

    Added zoho notification bash file

--- preBuildNotif.sh
+++ preBuildNotif.sh
@@ -6,18 +6,18 @@ webhook_url="https://cliq.zoho.com/company/840183019/api/v2/channelsbyname/testr
 # Define the message payload with formatting and bot name
 curl -X POST -H "Content-type: application/json" \
      "$webhook_url" \
-     -d '{
-     "bot": {
-    "name": "Automation Test Notification",
-    "image": "https://img.freepik.com/premium-vector/hazard-warning-attention-sign-with-exclamation-mark-symbol-white_231786-5218.jpg?w=360"
-  },
-       "text": "Running *'$JOB_BASE_NAME'* \nCheckout the logs here: *'$JOB_URL'*",
-       "broadcast": "true",
-       "card": {
-         "title": "[INFO] Starting automation test: ",
-         "theme": "modern-inline"
-       }  
-     }'
+     -d "{
+     \"bot\": {
+       \"name\": \"Automation Test Notification\",
+       \"image\": \"https://img.freepik.com/premium-vector/hazard-warning-attention-sign-with-exclamation-mark-symbol-white_231786-5218.jpg?w=360\"
+     },
+     \"text\": \"Running *'$JOB_BASE_NAME'* \nCheckout the logs here: *'$JOB_URL'*\",
+     \"broadcast\": \"true\",
+     \"card\": {
+       \"title\": \"[INFO] Starting automation test: \",
+       \"theme\": \"modern-inline\"
+     }  
+     }"
 
 # [ERROR] 😱 Test failed:
 # [INFO] 🎉 Test passed:
