0523adf94f85f89c28d0064ff74ddcdb0c835358
wdilidili01 <wdilidili@fullscale.ph>
2024-09-12 09:25:24 +0800

    update delete skill

--- pages/functions/settingsFunc.js
+++ pages/functions/settingsFunc.js
@@ -113,10 +113,11 @@ exports.SettingsPage = class SettingsPage {
     }
 
     async deleteSkill(testData) {
-        await this.actionDriver.waitElementUntilHidden(settingsLocators.skillName);
-        await this.actionDriver.setText(settingsLocators.searchSkill, newSkill);
+        await this.actionDriver.waitElementUntilVisible(settingsLocators.searchSkill);
+        await this.actionDriver.setText(settingsLocators.searchSkill, testData);
         await this.actionDriver.keyboardPress('Enter');
         await this.actionDriver.waitElementUntilHidden(settingsLocators.loadingRecords);
+        await this.actionDriver.waitElementUntilClickable(settingsLocators.deleteSkillList);
         await this.actionDriver.selectDataFromText(testData, settingsLocators.skillNameList, settingsLocators.deleteSkillList);
         await this.actionDriver.clickButton(settingsLocators.confirmDelete);
         await this.actionDriver.expectToHaveCount(settingsLocators.skillNameList, 0);
