517736d28f462517840b5272c9b359ba355ee789
wdilidili01 <wdilidili@fullscale.ph>
2024-09-25 07:14:00 +0800

    revert

--- tests/zoho.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test } from '@playwright/test';
-const { getLatestEmail } = require('../utils/zohoDriver.js')
-
-test('test Zoho123', async() => {
-    await getLatestEmail('Ann Pastoriza has share', 'notify@zohoworkdrive.com');
-})
