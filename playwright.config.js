e87fe3d59ac7e3af592da349661e6d32b4f8a684
wdilidili01 <wdilidili@fullscale.ph>
2024-10-02 09:30:21 +0800

    revert playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -30,6 +30,8 @@ module.exports = defineConfig({
   
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 1,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['playwright-json-summary-reporter'],
@@ -53,28 +55,7 @@ module.exports = defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { 
-        ...devices['Desktop Chrome'],
-        workers: undefined,
-      },
-      testMatch: [
-        '**/*.spec.ts',
-        '!**/4FindTalents/**',
-        '!**/8Feedback/8.1submitFeedback.spec.ts',
-        '!**/10EmployeeProfile/10.2addInterview.spec.ts',
-      ]
-    },
-    {
-      name: 'chromium',
-      use: { 
-        ...devices['Desktop Chrome'],
-        workers: 1,
-      },
-      testMatch: [
-        '**/4FindTalents/**/*.spec.ts',
-        '**/8Feedback/8.1submitFeedback.spec.ts',
-        '**/10EmployeeProfile/10.2addInterview.spec.ts',
-      ]
+      use: { ...devices['Desktop Chrome']},
     },
 
     // {
