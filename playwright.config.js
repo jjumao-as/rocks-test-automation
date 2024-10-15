74dc4f050b56242798976f1e9ec977bdd6d6b918
mdelfino426 <mdelfino@fullscale.ph>
2024-10-15 18:08:17 +0800

    ROCKS-3552_Add Work Experience - Playwright Config worker and project adjustments

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,7 @@ module.exports = defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  // workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['playwright-json-summary-reporter'],
@@ -65,6 +65,7 @@ module.exports = defineConfig({
       testIgnore : [
         '**/4FindTalents/**',
         '8Feedback/8.1submitFeedback.spec.ts',
+        '10EmployeeProfile/10.2addInterview.spec.ts'
       ]
     },
     {
@@ -75,7 +76,8 @@ module.exports = defineConfig({
       },
       testMatch : [
         '**/4FindTalents/**',
-        '8Feedback/8.1submitFeedback.spec.ts'
+        '8Feedback/8.1submitFeedback.spec.ts',
+        '10EmployeeProfile/10.2addInterview.spec.ts'
 
       ]
     },
