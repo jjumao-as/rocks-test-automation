754c60a8f737d2bcb586d629224a9cdd25bf0e3d
wdilidili01 <wdilidili@fullscale.ph>
2024-10-31 20:24:37 +0800

    rocks-3482 commit changes

--- pages/functions/clientFunc.js
+++ pages/functions/clientFunc.js
@@ -1,3 +1,4 @@
+import { expect } from '@playwright/test';
 const clientPageLoc = require('../locators/clientLoc');
 const ActionDriver = require('../../utils/ActionDriver');
 const loginLocators = require('../locators/loginLoc');
@@ -27,7 +28,7 @@ exports.ClientsPage = class ClientsPage {
         await this.actionDriver.clickButton(clientPageLoc.addContactBtn);
         await this.actionDriver.setText(clientPageLoc.contactName, testData.firstName + " " + testData.lastName);
         await this.actionDriver.setText(clientPageLoc.contactEmail, newEmail);
-        if(!emailAdd[1].includes('fullscale')){
+        if (!emailAdd[1].includes('fullscale')) {
             await this.actionDriver.waitElementUntilEnabled(clientPageLoc.enableLoginToggle);
             await this.changeStyle();
             await this.actionDriver.clickButton(clientPageLoc.enableLoginToggle);
@@ -58,14 +59,14 @@ exports.ClientsPage = class ClientsPage {
 
     async validateEmail(subject, from) {
         let emailContent;
-        for(let i=1; i<=3; i++){
+        for (let i = 1; i <= 3; i++) {
             emailContent = await googleAPI(subject, from);
-            if(emailContent !== null) {
+            if (emailContent !== null) {
                 break;
             }
         }
         const empty = emailContent === null ? true : false;
-        if(!empty) {
+        if (!empty) {
             await this.page.setContent(emailContent[0].body.html);
             const emailSubject = emailContent[0].subject;
             if (subject.includes('Welcome')) {
@@ -84,14 +85,14 @@ exports.ClientsPage = class ClientsPage {
 
     async validateZohoEmail(subject, from) {
         let emailContent;
-        for(let i=1; i<=3; i++){
+        for (let i = 1; i <= 3; i++) {
             emailContent = await getLatestEmail(subject, from);
-            if(emailContent !== null) {
+            if (emailContent !== null) {
                 break;
             }
         }
         const empty = emailContent === null ? true : false;
-        if(!empty) {
+        if (!empty) {
             await this.page.setContent(emailContent.content);
             const emailSubject = emailContent.subject;
             if (subject.includes('Welcome')) {
@@ -152,7 +153,7 @@ exports.ClientsPage = class ClientsPage {
             const textContent = await rows.nth(j).textContent();
             const correctedContent = textContent.trim();
             if (correctedContent === name) {
-                await this.actionDriver.expectEquals(status, `(${clientPageLoc.requestStatus})[${j+1}]`);
+                await this.actionDriver.expectEquals(status, `(${clientPageLoc.requestStatus})[${j + 1}]`);
             }
         }
     }
@@ -168,7 +169,7 @@ exports.ClientsPage = class ClientsPage {
             const textContent = await rows.nth(j).textContent();
             const correctedContent = textContent.replace(/Name\s*/g, '').trim();
             if (correctedContent === name) {
-                await this.actionDriver.expectEquals(status, `(${clientPageLoc.teamRequestListStatus})[${j+1}]`);
+                await this.actionDriver.expectEquals(status, `(${clientPageLoc.teamRequestListStatus})[${j + 1}]`);
             }
         }
     }
@@ -306,29 +307,205 @@ exports.ClientsPage = class ClientsPage {
         await this.validateNoTooltip();
     }
 
-    async navigateToMyProfile() {
-        await this.actionDriver.waitElementUntilClickable(clientPageLoc.myProfile);
-        await this.actionDriver.clickButton(clientPageLoc.myProfile);
+    async navigateToPage(pageName) {
+        await this.actionDriver.waitElementUntilVisible(clientPageLoc.clientPagesName);
+        await this.actionDriver.selectDataFromText(pageName, clientPageLoc.clientPagesName, clientPageLoc.clientPagesName);
     }
 
-    async navigateToMyContacts() {
-        await this.actionDriver.waitElementUntilClickable(clientPageLoc.myContacts);
-        await this.actionDriver.clickButton(clientPageLoc.myContacts);
+    async verifyURL(expectedURL) {
+        const currentURL = await this.page.url();
+        const url = new URL(currentURL);
+        const result = url.pathname + url.search === expectedURL ? true : false;
+        await this.actionDriver.expectTrue(result);
     }
 
-    async navigateToFindTalents() {
-        await this.actionDriver.waitElementUntilClickable(clientPageLoc.findTalent);
-        await this.actionDriver.clickButton(clientPageLoc.findTalent);
+    async validateMyProfilePage() {
+        await this.actionDriver.waitElementUntilVisible(clientPageLoc.technicalProfile);
+        await this.actionDriver.checkElementVisibility(clientPageLoc.technicalProfile);
     }
 
-    async navigateToDocuments() {
-        await this.actionDriver.waitElementUntilClickable(clientPageLoc.docsandAgreements);
-        await this.actionDriver.clickButton(clientPageLoc.docsandAgreements);
+    async validateMyContactPage() {
+        await this.actionDriver.waitElementUntilVisible(clientPageLoc.myContactsHeader);
+        await this.actionDriver.checkElementVisibility(clientPageLoc.myContactsHeader);
     }
 
-    async navigateToManageTeam() {
-        await this.actionDriver.waitElementUntilClickable(clientPageLoc.docsandAgreements);
-        await this.actionDriver.clickButton(clientPageLoc.docsandAgreements);
+    async validateFindTalentPage() {
+        await this.actionDriver.waitElementUntilVisible(clientPageLoc.findTalentHeader);
+        await this.actionDriver.checkElementVisibility(clientPageLoc.findTalentHeader);
     }
 
+    async validateDocumentsPage() {
+        await this.actionDriver.waitElementUntilVisible(clientPageLoc.clientDocumentsAgreementsHeader);
+        await this.actionDriver.checkElementVisibility(clientPageLoc.clientDocumentsAgreementsHeader);
+    }
+
+    async validateManageTeamPage() {
+        await this.actionDriver.waitElementUntilVisible(clientPageLoc.manageTeamHeader);
+        await this.actionDriver.checkElementVisibility(clientPageLoc.manageTeamHeader);
+    }
+
+    async validatePerformanceReviewPage() {
+        await this.actionDriver.waitElementUntilVisible(clientPageLoc.performanceReviewHeader);
+        await this.actionDriver.checkElementVisibility(clientPageLoc.performanceReviewHeader);
+    }
+
+    async validateDailyStatusPage() {
+        await this.actionDriver.waitElementUntilVisible(clientPageLoc.dailyStatusReportHeader);
+        await this.actionDriver.checkElementVisibility(clientPageLoc.dailyStatusReportHeader);
+    }
+
+    async validateDailyTimePage() {
+        await this.actionDriver.waitElementUntilVisible(clientPageLoc.dailyTimeClockReportHeader);
+        await this.actionDriver.checkElementVisibility(clientPageLoc.dailyTimeClockReportHeader);
+    }
+
+    async validateWeeklyTimePage() {
+        await this.actionDriver.waitElementUntilVisible(clientPageLoc.weeklyTimeReportHeader);
+        await this.actionDriver.checkElementVisibility(clientPageLoc.weeklyTimeReportHeader);
+    }
+
+    async validateTimeClockSummaryPage() {
+        await this.actionDriver.waitElementUntilVisible(clientPageLoc.timeClockSummaryHeader);
+        await this.actionDriver.checkElementVisibility(clientPageLoc.timeClockSummaryHeader);
+    }
+
+    async navigateToHome() {
+        await this.actionDriver.waitElementUntilClickable(clientPageLoc.homePageButton);
+        await this.actionDriver.clickButton(clientPageLoc.homePageButton);
+    }
+
+    async validateSideTabs(data) {
+        await this.actionDriver.waitElementUntilVisible(clientPageLoc.clientPagesName);
+        const textArray = await this.actionDriver.getTextArray(clientPageLoc.clientPagesName);
+        const visibleText = await data.every(item => textArray.includes(item));
+        await this.actionDriver.expectTrue(visibleText);
+    }
+
+    async validateFindAddTalent() {
+        await this.actionDriver.waitElementUntilVisible(clientPageLoc.findAddTalentBtn);
+        await this.actionDriver.checkElementVisibility(clientPageLoc.findAddTalentBtn);
+        await this.actionDriver.clickButton(clientPageLoc.findAddTalentBtn);
+    }
+
+    async validateBackToManageTeam() {
+        await this.actionDriver.checkElementVisibility(clientPageLoc.manageTeamBtn);
+        await this.actionDriver.clickButton(clientPageLoc.manageTeamBtn);
+    }
+
+    async validateTalentCount() {
+        await this.actionDriver.scrollToBottom(clientPageLoc.noMoreRecords);
+        const number = await this.actionDriver.getText(clientPageLoc.talentCount);
+        const formattedNumber = Number(number);
+        await this.actionDriver.expectToHaveCount(clientPageLoc.nameList, formattedNumber);
+    }
+
+    async validateAvailableTalentButtons() {
+        await this.actionDriver.clickButton(clientPageLoc.firstTalent);
+        await this.actionDriver.waitElementUntilVisible(clientPageLoc.saveSelectedProfile);
+        await this.actionDriver.checkElementVisibility(clientPageLoc.saveSelectedProfile);
+        await this.actionDriver.checkElementVisibility(clientPageLoc.bookaCall);
+        await this.actionDriver.checkElementVisibility(clientPageLoc.addToTeam);
+        await this.actionDriver.clickButton(clientPageLoc.closeModal);
+    }
+
+    async validateSavedSuggested(title) {
+        await this.actionDriver.scrollToBottom(clientPageLoc.heartBtn);
+        await this.actionDriver.waitElementUntilClickable(clientPageLoc.heartBtn);
+        const firstElement = await this.page.$(clientPageLoc.heartBtn);
+        if (firstElement) {
+            const attributeValue = await firstElement.getAttribute('title');
+            const containsWord = attributeValue && attributeValue.includes(title);
+            await this.actionDriver.expectTrue(containsWord);
+        }
+    }
+
+    async sortDate(dateList) {
+        const trimmedDates = dateList.map(date => date.trim());
+        const sortedDates = trimmedDates.sort((a, b) => {
+            return new Date(b) - new Date(a);
+        })
+        return sortedDates;
+    }
+
+    async sortDateRange(dateRange) {
+
+        const currentYear = new Date().getFullYear();
+
+        const parseDateRange = (range) => {
+            const [start, end] = range.split(' - ').map(date => new Date(`${date.trim()}, ${currentYear}`)); // Add year for parsing
+            return { start, end, range };
+        };
+
+        const sortedDateRanges = dateRange
+            .map(parseDateRange) // Convert ranges to objects
+            .sort((a, b) => a.start - b.start) // Sort by start date
+            .map(item => item.range); // Return back to original range format
+
+
+        return sortedDateRanges;
+    }
+
+    async validateSortedDate() {
+        const textArray = await this.actionDriver.getTextArray(clientPageLoc.dailyDate);
+        const sortedDates = await this.sortDate(textArray);
+
+        const areDatesEqual = textArray.length === sortedDates.length && textArray.every((date, index) => {
+            return new Date(date.trim()).getTime() === new Date(sortedDates[index].trim()).getTime();
+        });
+
+        await this.actionDriver.expectTrue(areDatesEqual);
+    }
+
+    async validateLatestReport() {
+        await this.actionDriver.waitElementUntilHidden(clientPageLoc.loadingRecords);
+        await this.actionDriver.waitElementUntilVisible(clientPageLoc.latestTimeClockDate);
+        const textArray = await this.actionDriver.getTextArray(clientPageLoc.timeClockDate);
+        const sortedDates = await this.sortDate(textArray);
+        await this.actionDriver.expectEquals(sortedDates[0], clientPageLoc.latestTimeClockDate);
+        await this.actionDriver.expectElementNotToBeEmpty(clientPageLoc.latestTimeIn);
+        await this.actionDriver.expectElementNotToBeEmpty(clientPageLoc.latestTimeOut);
+    }
+
+    async validateTimeClockReport() {
+        await this.actionDriver.waitElementUntilVisible(clientPageLoc.latestWeeklyTimeDate);
+        const textArray = await this.actionDriver.getTextArray(clientPageLoc.weeklyTimeDates);
+        const sortedDateRanges = await this.sortDateRange(textArray);
+        await this.actionDriver.expectEquals(sortedDateRanges[0], clientPageLoc.latestWeeklyTimeDate);
+        await this.actionDriver.clickButton(clientPageLoc.latestWeeklyTimeDate);
+        await this.actionDriver.checkAllElementsVisibility(clientPageLoc.employeeNameList);
+    }
+
+    async validateTimeClockSummaryReport(type) {
+        for(const t of type) {
+            await this.actionDriver.clickButton(clientPageLoc.exportCurrentRecordReportBtn);
+            if(t === 'csv') {
+                await this.actionDriver.clickButton(clientPageLoc.csvBtn);
+            } else {
+                await this.actionDriver.clickButton(clientPageLoc.xlsxBtn);
+            }
+
+            await this.downloadExportCurrentRecords(t);
+        }
+    }
+
+    async downloadExportCurrentRecords(type) {
+        function getCurrentDate() {
+            const now = new Date();
+            const year = now.getFullYear();
+            const month = String(now.getMonth() + 1).padStart(2, '0');
+            const day = String(now.getDate()).padStart(2, '0');
+            return `${year}-${month}-${day}`;
+        }
+
+        const [download] = await Promise.all([
+            this.page.waitForEvent('download'),
+            this.page.click(clientPageLoc.exportBtn)
+        ]);
+        const currentDate = getCurrentDate();
+        const downloadedFileName = download.suggestedFilename();
+        const expectedStartName = `monthly_tsheets_${currentDate}_`;
+        const expectedEndName = `.${type}`;
+
+        expect(downloadedFileName).toMatch(new RegExp(`^${expectedStartName}.*${expectedEndName}`));
+    }
 }
